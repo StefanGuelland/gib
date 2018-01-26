ac03c87e12862b21856e94784689e8e788673428
Stefan Guelland <stefan@sguelland.de>
2018-01-26 15:47:33 +0100

    app php db example

--- php/index.php
+++ php/index.php
@@ -1,9 +1,20 @@
 <html>
  <head>
+ <meta charset="Windows-1252">
   <title>PHP-Test</title>
  </head>
  <body>
 
- <?php echo '<p>Hallo Welt</p>'; ?>
+ <?php
+$pdo = new PDO('mysql:host=db;dbname=firmendb;charset=latin1', 'springuser', 'ThePassword');
+
+$stmt = $pdo->prepare("SELECT name FROM company LIMIT 50");
+$stmt->execute();
+
+$test = $stmt->fetchAll();
+
+var_dump($test);
+
+ echo '<p>Hallo Welt mit dbsdfsdf</p>'; ?>
  </body>
 </html>
